8532f30287857913d72958154817df0b1a74f310
stephy <stephshiro35@gmail.com>
2024-03-21 13:58:50 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+console.log(selectingDrivers[0](['Antonia', 'Nuru', 'Amari', 'Mo']));
+
+
+console.log(selectingDrivers[1](['Antonia', 'Nuru', 'Amari', 'Mo']));
+
+function createFareMultiplier(integer) {
+    return function(fare) {
+        return fare * integer;
+    };
+}
+
+const fareMultiplierBy4 = createFareMultiplier(4);
+
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, selectFunction) {
+    return selectFunction(drivers);
+}
+
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers)); 
+
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers)); 
